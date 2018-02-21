5c3142d6d29dd5c4e449ae5d22b39c9f5811853d
Michael Zeng <miczeng@microsoft.com>
2018-02-21 10:43:02 -0800

    Trust proxy

--- src/server/app.js
+++ src/server/app.js
@@ -39,6 +39,7 @@ app.use(function (req, res, next) {
 app.use(require('x-frame-options')());
 app.use(require('body-parser').json({ limit: '5mb' }));
 app.use(require('cookie-parser')());
+app.enable('trust proxy');
 var expressSession = require('express-session');
 var MongoStore = require('connect-mongo')(expressSession);
 
