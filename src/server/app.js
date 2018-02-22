3195be05c60f52bbe3586a38152b869790dc5dc0
Michael Tseng <miczeng@microsoft.com>
2018-02-21 16:38:56 -0800

    Add no-sniff MIME type protection. (#77)

--- src/server/app.js
+++ src/server/app.js
@@ -7,6 +7,7 @@ var passport = require('passport');
 var path = require('path');
 var sass_middleware = require('node-sass-middleware');
 var cleanup = require('./middleware/cleanup');
+var noSniff = require('dont-sniff-mimetype');
 // var sass_middleware = require('node-sass-middleware');
 
 // ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -39,6 +40,7 @@ app.use(function (req, res, next) {
 app.use(require('x-frame-options')());
 app.use(require('body-parser').json({ limit: '5mb' }));
 app.use(require('cookie-parser')());
+app.use(noSniff());
 app.enable('trust proxy');
 var expressSession = require('express-session');
 var MongoStore = require('connect-mongo')(expressSession);
